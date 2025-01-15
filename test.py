e90b05225bd91250ff3c397f6cbd1766123ca0a1
logan11 <llumetta@calpoly.edu>
2025-01-15 10:06:38 -0800

    message

--- test.py
+++ test.py
@@ -1,2 +1,30 @@
-#hello
-#new line
+from base64 import b64encode, b64decode
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+import json
+
+data = b""
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_CBC)
+ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+iv = b64encode(cipher.iv).decode('utf-8')
+ct = b64encode(ct_bytes).decode('utf-8')
+result = json.dumps({'iv':iv, 'ciphertext':ct})
+print(result)
+
+
+#decryption:
+try:
+    b64 = json.loads(result)
+    iv = b64decode(b64['iv'])
+    ct = b64decode(b64['ciphertext'])
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    pt = unpad(cipher.decrypt(ct), AES.block_size)
+    print("The message was: ", pt)
+except (ValueError, KeyError):
+    print("Incorrect decryption")
+
+
+
+
